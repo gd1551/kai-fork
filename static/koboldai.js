3c969b75d86e625d9e62ab602df51967b1519fbc
gd1551 <55545126+gd1551@users.noreply.github.com>
2023-02-28 00:05:02 +0300

    maybe fix scrolling

--- static/koboldai.js
+++ static/koboldai.js
@@ -495,7 +495,7 @@ function process_actions_data(data) {
 	}
 	
 	clearTimeout(game_text_scroll_timeout);
-	game_text_scroll_timeout = setTimeout(run_infinite_scroll_update.bind(null, action_type, actions, first_action), 0);
+	game_text_scroll_timeout = setTimeout(run_infinite_scroll_update.bind(null, action_type, actions, first_action), 50);
 	clearTimeout(auto_loader_timeout);
 
 	
