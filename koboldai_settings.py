3e03c73b34217711e6bb89ca6ba32d8755b44529
gd1551 <55545126+gd1551@users.noreply.github.com>
2023-03-01 00:34:17 +0300

    maybe fix newlines for real

--- koboldai_settings.py
+++ koboldai_settings.py
@@ -336,6 +336,10 @@ class koboldai_vars(object):
         # Always add newlines on chat v2
         if action_text_split and self.is_chat_v2():
             action_text_split[-1][0] = action_text_split[-1][0].strip() + "\n"
+
+        if action_text_split and len(action_text_split) > 0 and not self.is_chat_v2():
+            if action_text_split[-1][0][-1] == '\n':
+                action_text_split[-1][0] = action_text_split[-1][0][:-1]
         
         ######################################### Prompt ########################################################
         #Add prompt length/text if we're set to always use prompt
